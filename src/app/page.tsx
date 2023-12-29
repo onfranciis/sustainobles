006f6bc9938d4b1ab2e043db9d2bdedd8971fd16
onfranciis <onukwuf@gmail.com>
2023-12-29 15:23:55 +0100

    feat: create landing page part two

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import One from "@/components/landingPage/One";
+import Two from "@/components/landingPage/Two";
 
 export default function Home() {
   return (
     <main className=" min-h-screen ">
       <One />
+      <Two />
     </main>
   );
 }
---
+++ src/components/landingPage/Two.tsx
@@ -0,0 +1,28 @@
+import { Poppins } from "next/font/google";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["700", "600", "500", "400", "300", "200"],
+});
+
+const Two = () => {
+  return (
+    <div className={poppins.className}>
+      <div className="relative h-[376px] ">
+        <div className="bg-[url('/landing-two.png')] bg-no-repeat bg-center bg-cover w-[calc(100%-200px)] absolute top-1/2 p-8 pr-[73px] pb-12 rounded-2xl left-0 right-0 ml-auto mr-auto">
+          <p className="mb-[18px] text-6xl font-bold">Our Purpose</p>
+
+          <p className="font-medium text-[40px]">
+            Empowering Children in disadvantaged areas by championing recycling
+            initiatives, organizing book clubs and using the earning to fund
+            their education. Also, harnessing and leveraging the power of
+            football to positively impact and transform their lives.
+          </p>
+        </div>
+      </div>
+      <div className="bg-white h-[376px]"></div>
+    </div>
+  );
+};
+
+export default Two;
