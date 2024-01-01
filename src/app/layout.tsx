94480538029bf9f6f197b34412bd60de4f555600
onfranciis <onukwuf@gmail.com>
2024-01-01 11:26:48 +0100

    fix: update OG

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,33 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
 import NavBar from "@/components/navBar/NavBar";
 import Footer from "@/components/footer/Footer";
+import "./globals.css";
+import OG from "@/app/opengraph-image.png";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Sustainobles",
   description: "",
+  openGraph: {
+    images: [
+      {
+        url: OG.src,
+        width: OG.width,
+        height: OG.height,
+      },
+    ],
+  },
+  twitter: {
+    images: [
+      {
+        url: OG.src,
+        width: OG.width,
+        height: OG.height,
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
