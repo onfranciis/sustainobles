4600f46f1e85f009ebe07af0985cb21ecf862645
onfranciis <onukwuf@gmail.com>
2024-01-08 21:03:04 +0100

    fix: update requested changes on about

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -2,18 +2,21 @@ import Image from "next/image";
 import happy from "../../assets/happy.png";
 import schlOne from "../../assets/school-one.png";
 import schlTwo from "../../assets/school-two.png";
+import { poppins } from "@/components/landingPage/Two";
 
 export default function Home() {
   return (
     <main className="min-h-screen py-14 px-14 bg-[#F6FFDC] text-white">
       <div
-        style={{ backgroundImage: `url(${schlOne.src})` }}
-        className="min-h-[400px] mb-12 p-6 pb-20 relative bg-cover bg-center rounded-2xl"
+        style={{ backgroundImage: `url("/about-one.png")` }}
+        className="min-h-[400px] mb-12 p-6 pb-20 relative bg-cover bg-center rounded-2xl bg-fixed"
       >
         <h2 className="mb-4 text-[64px] -lg:text-[52px] -md:text-[40px] font-bold uppercase">
           Completed Projects
         </h2>
-        <p className="text-[40px] -lg:text-4xl -md:text-2xl leading-normal">
+        <p
+          className={`text-[36px] -lg:text-4xl -md:text-2xl leading-normal opacity-80 ${poppins.className}`}
+        >
           Since our inception, Sustainobles have engaged in and completed many
           sustainable projects and we can pride ourselves and also take pride in
           our sponsors and volunteers as growing...
@@ -24,13 +27,15 @@ export default function Home() {
       </div>
 
       <div
-        style={{ backgroundImage: `url(${schlTwo.src})` }}
-        className="min-h-[400px] mb-12 p-6 pb-20 relative bg-cover bg-center rounded-2xl"
+        style={{ backgroundImage: `url("about-two.png")` }}
+        className="min-h-[400px] mb-12 p-6 pb-20 relative bg-cover bg-center rounded-2xl bg-fixed"
       >
         <h2 className="mb-4 text-[64px] -lg:text-[52px] -md:text-[40px] font-bold uppercase">
           Ongoing Projects
         </h2>
-        <p className="text-[40px] -lg:text-4xl -md:text-2xl leading-normal">
+        <p
+          className={`text-[36px] -lg:text-4xl -md:text-2xl leading-normal opacity-80 ${poppins.className}`}
+        >
           Since our inception, Sustainobles have engaged in and completed many
           sustainable projects and we can pride ourselves and also take pride in
           our sponsors and volunteers as growing...
@@ -42,12 +47,12 @@ export default function Home() {
 
       <div
         style={{ backgroundImage: `url(${happy.src})` }}
-        className="min-h-[400px] p-6 pb-20 relative bg-cover bg-center rounded-2xl"
+        className="min-h-[400px] p-6 pb-20 relative bg-cover bg-center rounded-2xl bg-fixed"
       >
         <h2 className="mb-4 text-[64px] -lg:text-[52px] -md:text-[40px] font-bold uppercase">
           Upcoming Projects
         </h2>
-        <p className="text-[40px] -lg:text-4xl -md:text-2xl leading-normal"></p>
+        <p className="text-[36px] -lg:text-4xl -md:text-2xl leading-normal"></p>
         <button className="py-3 px-12 absolute bottom-6 right-6 rounded-xl bg-[#8AB440] text-[#1D401D]">
           See More
         </button>
