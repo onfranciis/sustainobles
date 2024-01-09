0f619569b495713ce161950f91ee8560ac3e9bad
onfranciis <onukwuf@gmail.com>
2024-01-09 20:58:57 +0100

    feat: handle error

--- src/app/projects/@modal/(.)gallery/[id]/page.tsx
+++ src/app/projects/@modal/(.)gallery/[id]/page.tsx
@@ -7,15 +7,17 @@ import { createPortal } from "react-dom";
 const GalleryPage = ({ params }: { params: { id: string } }) => {
   const Selected = GallerySections.find(
     (section) => section.slug === params.id
-  ) || { slug: "not-found", number: 0 };
-
-  return createPortal(
-    <ModalProjects
-      directory={Selected.slug}
-      numberOfImages={Selected.number}
-    />,
-    document.getElementById("modal-root")!
   );
+
+  return Selected
+    ? createPortal(
+        <ModalProjects
+          directory={Selected.slug}
+          numberOfImages={Selected.number}
+        />,
+        document.getElementById("modal-root")!
+      )
+    : null;
 };
 
 export default GalleryPage;
--- src/app/projects/gallery/[id]/page.tsx
+++ src/app/projects/gallery/[id]/page.tsx
@@ -4,9 +4,16 @@ import { GallerySections } from "@/utils/Gallery";
 const GalleryDefault = ({ params }: { params: { id: string } }) => {
   const Selected = GallerySections.find(
     (section) => section.slug === params.id
-  ) || { slug: "not-found", number: 0 };
+  );
 
-  return <Gallery directory={Selected.slug} numberOfImages={Selected.number} />;
+  return Selected ? (
+    <Gallery directory={Selected.slug} numberOfImages={Selected.number} />
+  ) : (
+    <div className=" min-h-screen flex flex-col justify-center items-center p-5 bg-white text-primary">
+      <p className="text-5xl font-bold">404</p>
+      <p className="text-3xl">Not Found</p>
+    </div>
+  );
 };
 
 export default GalleryDefault;
