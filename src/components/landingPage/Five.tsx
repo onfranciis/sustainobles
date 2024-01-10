1c194b70173f7048d131d21cbbe5c62a890980de
petemz <peteremmies7@gmail.com>
2024-01-10 12:52:01 +0100

    updates on responsivness of the homapage

--- src/components/landingPage/Five.tsx
+++ src/components/landingPage/Five.tsx
@@ -5,11 +5,11 @@ const Five = () => {
   return (
     <div className="bg-white pb-[22px] px-16 -md:px-12 -sm:px-8 -xs:px-5">
       <p
-        className={`${poppins.className} text-[64px] -lg:text-[52px] -md:text-[40px] font-bold text-[#1E1E1E] mb-6`}
+        className={`${poppins.className} text-[64px] -lg:text-[52px] -md:text-[40px] -sm:text-4xl font-bold text-[#1E1E1E] mb-6`}
       >
         Ongoing Projects
       </p>
-      <div className="flex overflow-x-auto gap-10 pb-2 px-[5px] -md:px-[50px]">
+      <div className="flex overflow-x-auto gap-10 pb-2 sm:px-7 md:px-[50px]">
         <ProjectCard />
         <ProjectCard />
         <ProjectCard />
